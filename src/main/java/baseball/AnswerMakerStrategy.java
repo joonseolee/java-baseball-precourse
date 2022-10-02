96b4ee994d66c16f725e661540ea231bec69eee1
joonseolee <niceweather94@gmail.com>
2022-10-02 10:31:09 +0900

    feat: add getAnswer method

--- src/main/java/baseball/AnswerMakerStrategy.java
+++ src/main/java/baseball/AnswerMakerStrategy.java
@@ -3,4 +3,6 @@ package baseball;
 public interface AnswerMakerStrategy<T> {
 
     T generateAnswer();
+
+    int getAnswerSize();
 }
--- src/main/java/baseball/RangeNumberAnswerMakerStrategy.java
+++ src/main/java/baseball/RangeNumberAnswerMakerStrategy.java
@@ -2,6 +2,9 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RangeNumberAnswerMakerStrategy implements AnswerMakerStrategy<Integer> {
     private static final int ANSWER_START_RANGE = 1;
     private static final int ANSWER_END_RANGE = 9;
@@ -11,14 +14,36 @@ public class RangeNumberAnswerMakerStrategy implements AnswerMakerStrategy<Integ
 
     @Override
     public Integer generateAnswer() {
-        StringBuilder answer = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>();
 
-        for (int index = 0 ; index < this.answerSize ; index++) {
-            int number = Randoms.pickNumberInRange(ANSWER_START_RANGE, ANSWER_END_RANGE);
-            answer.append(number);
+        while (numbers.size() != this.answerSize) {
+            int uniqueNumber = generateUniqueNumber(numbers, Randoms.pickNumberInRange(ANSWER_START_RANGE, ANSWER_END_RANGE));
+            numbers.add(uniqueNumber);
         }
 
-        return Integer.parseInt(answer.toString());
+        return toAnswerInteger(numbers);
+    }
+
+    private int generateUniqueNumber(List<Integer> numbers, int randomNumber) {
+        if (numbers.contains(randomNumber)) {
+            return generateUniqueNumber(numbers, Randoms.pickNumberInRange(ANSWER_START_RANGE, ANSWER_END_RANGE));
+        }
+
+        return randomNumber;
+    }
+
+    private Integer toAnswerInteger(List<Integer> numbers) {
+        StringBuilder builder = new StringBuilder();
+        for (int index = 0 ; index < numbers.size() ; index++) {
+            builder.append(numbers.get(index));
+        }
+
+        return Integer.parseInt(builder.toString());
+    }
+
+    @Override
+    public int getAnswerSize() {
+        return answerSize;
     }
 
     public RangeNumberAnswerMakerStrategy() {
